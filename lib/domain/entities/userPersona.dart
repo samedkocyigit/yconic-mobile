4a09fba0555128ad6ea366317e22551a091a5ef5
sam <abdulsamed.kcygt@gmail.com>
2025-03-23 18:22:37 +0300

    userPersona entity has been created

---
+++ lib/domain/entities/userPersona.dart
@@ -0,0 +1,11 @@
+class Userpersona {
+  final String Id;
+  final int UserType;
+  final String UserId;
+
+  Userpersona({
+    required this.Id,
+    required this.UserType,
+    required this.UserId
+  });
+}
