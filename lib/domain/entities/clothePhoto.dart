e9ff174db3e19c9130c957d6bc1f9e86a2494d9a
sam <abdulsamed.kcygt@gmail.com>
2025-03-23 18:21:42 +0300

    clothePhoto entity has been created

---
+++ lib/domain/entities/clothePhoto.dart
@@ -0,0 +1,11 @@
+class Clothephoto {
+  final String Id;
+  final String Url;
+  final String ClotheId;
+
+  Clothephoto({
+    required this.Id,
+    required this.Url,
+    required this.ClotheId
+  });
+}
