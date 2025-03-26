d0b519e79dca7ac008f7c537b0acd5249c6b8a7a
sam <abdulsamed.kcygt@gmail.com>
2025-03-26 17:09:37 +0300

    splash screen refactored

---
+++ lib/presentation/screens/splash_screen.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yconic/presentation/providers/token_provider.dart';
+import 'package:yconic/presentation/screens/auth/login_screen.dart';
+import 'package:yconic/presentation/screens/auth/register_screen.dart';
+import 'package:yconic/presentation/screens/home/home_screen.dart';
+
+class SplashScreen extends ConsumerStatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends ConsumerState<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _checkAuth();
+  }
+
+  Future<void> _checkAuth() async {
+    final tokenService = ref.read(tokenServiceProvider);
+    final token = await tokenService.getToken();
+
+    if (token != null && token.isNotEmpty) {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => LoginScreen()),
+      );
+    } else {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (_) => HomeScreen()),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: CircularProgressIndicator()),
+    );
+  }
+}
