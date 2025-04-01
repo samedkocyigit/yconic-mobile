1f29c89c5df3d2a249e812fbed28816557792fb3
sam <abdulsamed.kcygt@gmail.com>
2025-04-02 01:32:33 +0300

    properties refactored

--- lib/domain/entities/garderobe.dart
+++ lib/domain/entities/garderobe.dart
@@ -2,13 +2,13 @@ import 'package:yconic/domain/entities/clotheCategory.dart';
 
 class Garderobe {
   final String Id;
-  final String Name;
+  final String? Name;
   final String UserId;
   final List<ClotheCategory> ClothesCategories;
 
   Garderobe(
       {required this.Id,
-      required this.Name,
+      this.Name,
       required this.UserId,
       required this.ClothesCategories});
 
